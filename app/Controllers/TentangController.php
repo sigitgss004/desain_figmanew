54f422c0fc6cca07691f1d8cd9e3c3b497c2de6a
sigitgss004 <sigitgss004@gmail.com>
2024-10-28 16:19:09 +0700

    28/10/2024

---
+++ app/Controllers/TentangController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\TentangModel;
+
+
+class TentangController extends BaseController
+{
+    public function tentang()
+    {
+         // Inisialisasi model
+         $tentangModel = new TentangModel();
+        
+         // Ambil semua data, data akan dikembalikan sebagai object
+         $data['tentang'] = $tentangModel->getTentang();
+         
+         // Kirim data ke view
+         return view('Tentang', $data);
+    }
+}
